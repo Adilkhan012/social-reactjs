1a0a02bbda835d63fd2ff717c061545da6090d42
Adilkhan012 <khanadil123457@gmail.com>
2023-01-19 23:49:51 +0500

    connect wallet

--- src/component/ConnectWalletButton.js
+++ src/component/ConnectWalletButton.js
@@ -1,29 +1,19 @@
 import styles from "../styles/ConnectWallet.module.css";
-import React, { Component }  from 'react';
+import React, { Component } from "react";
 
-const ConnectWalletButton = ({
-  onPressLogout,
-  onPressConnect,
-  loading,
-  address,
-}) => {
+const ConnectWalletButton = ({ onPressLogout, onPressConnect, loading, address }) => {
   return (
     <div>
       {address && !loading ? (
         <button onClick={onPressLogout} className={styles["connect-wallet"]}>
-          {address.substring(0, 6) +
-            '...' +
-            address.substring(address.length - 4, address.length)}
+          {address.substring(0, 6) + "..." + address.substring(address.length - 4, address.length)}
         </button>
       ) : loading ? (
-        <button
-          className={`${styles["connect-wallet"]} ${styles["connect-button-loading"]}`}
-          disabled
-        >
+        <button className={`${styles["connect-wallet"]} ${styles["connect-button-loading"]}`} disabled>
           <div>Loading...</div>
         </button>
       ) : (
-        <button onClick={onPressConnect} className={styles['connect-wallet']}>
+        <button onClick={onPressConnect} className={styles["connect-wallet"]}>
           Connect Wallet
         </button>
       )}
