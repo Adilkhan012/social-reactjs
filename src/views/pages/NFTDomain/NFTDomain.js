9ff68c42764ec11df2577db86b63df724ecb33bc
Adilkhan012 <khanadil123457@gmail.com>
2023-05-14 16:11:47 +0500

    pic for domain part

--- src/views/pages/NFTDomain/NFTDomain.js
+++ src/views/pages/NFTDomain/NFTDomain.js
@@ -4,7 +4,7 @@ import Web3 from "web3";
 import contractABI from "./contractABI";
 import "./styles.css";
 import ArialFont from "./Arialn.ttf";
-import LaziImage from "./lazi.jpeg";
+import LaziImage from "./lazi.png";
 
 import { AuthContext } from "src/context/Auth";
 import { MdPhoto, MdAddToPhotos } from "react-icons/md";
